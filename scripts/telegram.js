ac4d22eb8f194974af02698d441eb472447820a9
Rodolfo Robles <rodolforoblesramos@gmail.com>
2024-06-23 12:35:52 -0600

    use new post function for telegram and separate stylesheet

--- scripts/telegram.js
+++ scripts/telegram.js
@@ -1,15 +1,18 @@
-import { getTelegramMessage, getTelegramCommand} from "../utils/telegram_updates.js";
+import { getTelegramMessage, getTelegramCommand } from "./telegram_updates.js";
+import { postAndRetrieve } from "../utils/general.js";
 
 const form = document.getElementById('form-to-aws');
-const successMessage = document.querySelector('.success');
+const successMessage = document.querySelector('.success-message');
 const lastRecordsButton = document.querySelector('.js-last-records');
+const textArea = document.querySelector('.js-text-area');
+
 
 //ENDPOINT
-function buildUrl () {
-const urlCode = document.getElementById('url-code').value;
-const last = urlCode.substr(-2);
-const apiEndpoint = `https://iek42z${last}tm4ip27cnkf4bw3${urlCode.substr(0,3)}0dzpyd.lambda-url.eu-central-1.on.aws/`
-return apiEndpoint
+function buildUrl() {
+  const urlCode = document.getElementById('url-code').value;
+  const last = urlCode.substr(-2);
+  const apiEndpoint = `https://iek42z${last}tm4ip27cnkf4bw3${urlCode.substr(0, 3)}0dzpyd.lambda-url.eu-central-1.on.aws/`
+  return apiEndpoint
 }
 
 
@@ -32,62 +35,42 @@ form.addEventListener('submit', async (event) => {
   const description = document.getElementById('row-desc').value;
   const cost = document.getElementById('row-cost').value;
   const urlCode = document.getElementById('url-code').value;
+
   if (description && cost && urlCode) {
     const concatenatedMessage = `${description} ${cost}`;
-
     const updateObject = getTelegramMessage(concatenatedMessage);
-    
+
     try {
-      const response = await fetch(buildUrl(), {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json'
-        },
-        body: JSON.stringify(updateObject)
-      });
-      
-      if (response.ok) {
-        const responseData = await response.text();
-        console.log(responseData);
-        successMessage.textContent = responseData + ' ' + description + ' ' + cost;
-        successMessage.style.color = 'green';
-      } else {
-        successMessage.textContent = 'Failed to send message.';
-        successMessage.style.color = 'red';
-      }
+      const responseData = await postAndRetrieve(buildUrl(), updateObject, 'text');
+      console.log(responseData);
+      updateSuccessMessage(responseData, description, cost, 'green');
     } catch (error) {
-      successMessage.textContent = 'Error: ' + error.message;
-      successMessage.style.color = 'red';
+      updateSuccessMessage(error.message, '', '', 'red');
     }
   } else {
-    successMessage.textContent = 'Please fill out all fields.';
-    successMessage.style.color = 'red';
+    updateSuccessMessage('Please fill out all fields.', '', '', 'red');
   }
 });
 
-
-lastRecordsButton.addEventListener('click', async () => {
-  
-  const updateObject = getTelegramCommand('last_records');
-  try {
-  const response = await fetch(buildUrl(),{
-    method: 'POST',
-    headers:  {
-      'Content-Type': 'application/json'
-    },
-    body: JSON.stringify(updateObject)
-  });
-
-  const responseData = await response.text();
-  console.log(responseData);
-  if (responseData === 'Success') {
-    showSuccessCheckmark();
-  }
-
-} catch (error) {
-  textArea.textContent = 'Error: ' + error.message;
-  setTimeout(() => textArea.textContent = "",1000)
+function updateSuccessMessage(message, description, cost, color) {
+  console.log(description + cost)
+  successMessage.textContent = message + ' ' + description + ' ' + cost;
+  successMessage.style.color = color;
 }
+
+
+lastRecordsButton.addEventListener('click', async (event) => {
+
+  try {
+    const responseData = await postAndRetrieve(buildUrl(), getTelegramCommand('last_records'),'text')
+    console.log(responseData);
+    if (responseData === 'Success') {
+      showSuccessCheckmark();
+    }
+  } catch (error) {
+    textArea.textContent = 'Error: ' + error.message;
+    setTimeout(() => textArea.textContent = "", 2000)
+  }
 }
 );
 
