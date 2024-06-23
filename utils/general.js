3dd9015e7653f117445606eed456a7b0a2dac707
Rodolfo Robles <rodolforoblesramos@gmail.com>
2024-06-23 12:33:25 -0600

    create function to post data

--- utils/general.js
+++ utils/general.js
@@ -1,55 +1,22 @@
-function generateHeader() {
-  const header = document.getElementById('same-header');
-  header.innerHTML = `
-      <div class="header-container">
-          <div class="logo">RRR</div>
-          <nav class="nav">
-              <a href="index.html">Home</a>
-              <a href="telegram.html">Telegram</a>
-              <a href="#contact">Contact</a>
-              <a href="secondary.html">Test</a>
-          </nav>
-      </div>
-  `;
 
-  const style = document.createElement('style');
-  style.innerHTML = `
-      body {
-          margin: 0;
-          font-family: Arial, sans-serif;
-      }
-      .header {
-          background-color: #004481; /* BBVA dark blue */
-          color: white;
-          padding: 20px 0;
-      }
-      .header-container {
-          display: flex;
-          justify-content: space-between;
-          align-items: center;
-          max-width: 1200px;
-          margin: 0 auto;
-          padding: 0 20px;
-      }
-      .logo {
-          font-size: 24px;
-          font-weight: bold;
-      }
-      .nav {
-          display: flex;
-          gap: 20px;
-      }
-      .nav a {
-          color: white;
-          text-decoration: none;
-          font-size: 18px;
-          transition: color 0.3s;
-      }
-      .nav a:hover {
-          color: #00b2e6; /* BBVA light blue */
-      }
-  `;
-  document.head.appendChild(style);
+export async function postAndRetrieve(url, data, result = 'json') {
+  try {
+    const response = await fetch(url, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(data)
+    });
+
+    let responseData;
+    if (result === 'json') {
+      responseData = await response.json();
+    } else if (result === 'text') {
+      responseData = await response.text();
+    }
+    return responseData;
+  } catch (error) {
+    throw new Error('The following error occurred: ' + error)
+  }
 }
-
-generateHeader();
